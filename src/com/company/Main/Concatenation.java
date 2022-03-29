21242f1fed66014afed41295e0a1e8b794c1e3c6
Hello Vistula 2 <zjilani1@stu.vistula.edu.pl>
2022-03-29 13:00:36 +0200

    Task_4

---
+++ src/com/company/Main/Concatenation.java
@@ -0,0 +1,31 @@
+package com.company.Main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Concatenation {
+    public static void main(String[] args) throws IOException {
+
+        String sen1, sen2,join;
+        boolean check;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter first sentence:- ");
+        sen1 = br.readLine();
+        System.out.println("Enter second sentence:- ");
+        sen2 = br.readLine();
+
+        join = sen1.concat(" " + sen2);
+        System.out.println("\nThe concatenated sentence is: " + join);
+
+        check = sen1.equals(sen2);
+        System.out.println("Comparing: " + check);
+        if (check == true){
+            System.out.println("The strings Match!");
+        }
+        else {
+            System.out.println("The strings are alternating!");
+        }
+    }
+}
