0fc9f36fa985fc9db7bf9dd0510af46d601bdb0a
Hello Vistula 2 <zjilani1@stu.vistula.edu.pl>
2022-03-29 11:26:44 +0200

    Task_1

---
+++ src/com/company/Main/FillingArray.java
@@ -0,0 +1,15 @@
+package com.company.Main;
+
+public class FillingArray {
+    public static void main(String[] args) {
+
+        int[] elements = new int[10];
+
+        for (int i = 0; i < elements.length; i++){
+            elements[i] = elements.length - 1 - i;
+        }
+        for (int x:elements){
+            System.out.println("Array[" + x + "] = " + elements[x]);
+        }
+    }
+}
